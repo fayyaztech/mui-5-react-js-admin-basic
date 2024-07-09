7cd5f460125d4cde3f0d5550cc73b5377d6bb5ea
Rudra <rudrakshnile930@gmail.com>
2024-07-09 16:17:18 +0530

    Add Student page added

--- src/layouts/routes.js
+++ src/layouts/routes.js
@@ -1,59 +1,64 @@
 // menuConfig.js
-import { Inbox, Mail } from '@mui/icons-material';
+import { Inbox, Mail } from "@mui/icons-material";
 
 const menuConfig = [
-    {
-        title: "Dashboard",
-        path: "/dashboard",
-        icon: <Inbox />,
-        children: []
-    },
-    {
-        title: "Students",
-        path: "#",
-        icon: <Inbox />,
-        children: [
-            {
-                title: "List",
-                path: "/students/list",
-                icon: <Mail />
-            },
-            {
-                title: "Attendance",
-                path: "/students/attendance",
-                icon: <Mail />
-            }
-        ]
-    },
-    {
-        title: "Demo",
-        path: "#",
-        icon: <Inbox />,
-        children: [
-            {
-                title: "List",
-                path: "/students/list",
-                icon: <Mail />
-            },
-            {
-                title: "Attendance",
-                path: "/students/attendance",
-                icon: <Mail />
-            }
-        ]
-    },
-    {
-        title: "Account",
-        path: "/account",
-        icon: <Inbox />,
-        children: []
-    },
-    {
-        title: "About",
-        path: "/about",
-        icon: <Inbox />,
-        children: []
-    }
+  {
+    title: "Dashboard",
+    path: "/dashboard",
+    icon: <Inbox />,
+    children: [],
+  },
+  {
+    title: "Students",
+    path: "#",
+    icon: <Inbox />,
+    children: [
+      {
+        title: "List",
+        path: "/students/list",
+        icon: <Mail />,
+      },
+      {
+        title: "Add Student",
+        path: "/students/add",
+        icon: <Mail />,
+      },
+      {
+        title: "Attendance",
+        path: "/students/attendance",
+        icon: <Mail />,
+      },
+    ],
+  },
+  {
+    title: "Demo",
+    path: "#",
+    icon: <Inbox />,
+    children: [
+      {
+        title: "List",
+        path: "/students/list",
+        icon: <Mail />,
+      },
+      {
+        title: "Attendance",
+        path: "/students/attendance",
+        icon: <Mail />,
+      },
+    ],
+  },
+  {
+    title: "Account",
+    path: "/account",
+    icon: <Inbox />,
+    children: [],
+  },
+  {
+    title: "About",
+    path: "/about",
+    icon: <Inbox />,
+    children: [],
+  },
 ];
 
 export default menuConfig;
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,45 +1,83 @@
-import { Box, Button, Card, CardContent, CardMedia, TextField, Typography } from '@mui/material'
-import React from 'react'
-import logo from '../logo.svg';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import logo from "../logo.svg";
 
 export default function Login() {
-    return (
-        <React.Fragment>
-            <Box sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                minHeight: '40vh'
-            }}>
-                <Card sx={{ maxWidth: 400 }}>
-                    <CardMedia
-                        component="img"
-                        image={logo}
-                        alt="logo"
-                    />
-                    <CardContent>
-                        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                            <Typography variant="h2" component="h1">
-                                Login
-                            </Typography>
-                        </Box>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center',
-                                '& > *': {
-                                    m: 1,
-                                },
-                            }}
-                        >
-                            <TextField id="username" label="Username" variant="outlined" type='text' fullWidth />
-                            <TextField id="password" label="Password" variant="outlined" type='password' fullWidth />
-                            <Button variant="contained">Login</Button>
-                        </Box>
-                    </CardContent>
-                </Card>
+  return (
+    <React.Fragment>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "40vh",
+        }}
+      >
+        <Card sx={{ maxWidth: 400, mt: 10, paddingX: 6 }}>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="30vh"
+          >
+            <CardMedia
+              component="img"
+              image={logo}
+              alt="logo"
+              sx={{ width: 200 }}
+            />
+          </Box>
+          <CardContent>
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+              <Typography
+                variant="h2"
+                component="h1"
+                sx={{ fontSize: "h4.fontSize" }}
+              >
+                Login
+              </Typography>
             </Box>
-        </React.Fragment>
-    )
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                "& > *": {
+                  m: 1,
+                },
+              }}
+            >
+              <TextField
+                id="username"
+                label="Username"
+                variant="outlined"
+                type="text"
+                fullWidth
+                sx={{ m: 1 }}
+              />
+              <TextField
+                id="password"
+                label="Password"
+                variant="outlined"
+                type="password"
+                fullWidth
+                sx={{ m: 1 }}
+              />
+              <Button variant="contained" sx={{ m: 2 }}>
+                Login
+              </Button>
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
+    </React.Fragment>
+  );
 }
